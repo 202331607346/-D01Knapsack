ff23ca306561a87e07a4e13b0e37699b5382930b
202331607346 <2509495906@qq.com>
2026-03-22 20:52:52 +0800

    修复: 解决matplotlib中文显示乱码问题

--- src/plot_draw.py
+++ src/plot_draw.py
@@ -1,9 +1,10 @@
-# src/plot_draw.py 完整修复版
 import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
 
+# 新增下面两行
 plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
-plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
-
+plt.rcParams['axes.unicode_minus'] = False
 
 def draw_scatter(itemsets):
     weights = []
@@ -12,11 +13,11 @@ def draw_scatter(itemsets):
         w1, v1, w2, v2, w3, v3 = item
         weights.extend([w1, w2, w3])
         values.extend([v1, v2, v3])
-
-    plt.figure(figsize=(8, 5))
+    
+    plt.figure(figsize=(8,5))
     plt.scatter(weights, values, c='blue', alpha=0.7)
     plt.xlabel("重量")
     plt.ylabel("价值")
-    plt.title("D{0-1}背包数据散点图")
+    plt.title("D{0-1}背包物品分布散点图")
     plt.grid(True)
     plt.show()
