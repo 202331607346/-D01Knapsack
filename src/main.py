52af75fa9f3dc74641e39bec294021d2a1de92a3
202331607346 <2509495906@qq.com>
2026-03-22 20:58:42 +0800

    优化: 完善程序入口结构，增加工作目录打印

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-# src/main.py 完整正确代码
-from knapsack import d01_knapsack_solve
-from file_io import read_data, save_txt, save_excel
+import os
+print("工作目录:", os.getcwd())
+
+from knapsack import dpl_solve
+from file_io import read_data, save_txt
 from plot_draw import draw_scatter
 from ui import KnapsackUI
 import tkinter as tk
 
-# 主程序
 if __name__ == "__main__":
     root = tk.Tk()
-    app = KnapsackUI(root, d01_knapsack_solve, read_data, save_txt, draw_scatter)
+    app = KnapsackUI(root, dpl_solve, read_data, save_txt, draw_scatter)
     root.mainloop()
